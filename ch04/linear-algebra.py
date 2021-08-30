f203c563273da9b7a6c398f17e45586ca0d42be5
jcanaviri <jcanaviri20@gmail.com>
2021-08-29 23:27:19 -0400

    ch04 Matrices

--- ch04/linear-algebra.py
+++ ch04/linear-algebra.py
@@ -91,3 +91,53 @@ def distance(v: Vector, w: Vector) -> float:
     return magnitude(substract(v, w))
 
 assert distance([5, 3], [1, 1]) == math.sqrt(20)
+
+
+# Matrices
+Matrix = List[List[float]]
+
+A = [[1, 2, 3],     # A has 2 rows and 3 columns
+    [4, 5, 6]]
+
+B = [[1, 2],        # B has 3 rows and 2 columns
+     [3, 4],
+     [5, 6]]
+
+from typing import Tuple
+
+def shape(A: Matrix) -> Tuple[int, int]:
+    """Returns (# of rows A, # of columns of A)"""
+    num_rows = len(A)
+    num_cols = len(A[0]) if A else 0
+    return num_rows, num_cols
+
+assert shape([[1, 2, 3], [4, 5, 6]]) == (2, 3) # Means 2 rows and 3 columns
+
+def get_row(A: Matrix, i: int) -> Vector:
+    """Returns the i-th row of A (as a Vector)"""
+    return A[i]
+
+def get_column(A: Matrix, j: int) -> Vector:
+    """Returns the j-th column of A (as a Vector)"""
+    return [A_i[j] for A_i in A]
+
+from typing import Callable
+
+def make_matrix(num_rows: int, 
+                num_cols: int, 
+                entry_fn: Callable[[int, int], float]) -> Matrix:
+    """Returns a num_rows x num_cols matrix whose (i, j)-th 
+    entry is entry_fn(i, j)"""
+    return [[entry_fn(i, j) 
+            for j in range(num_cols)] 
+            for i in range(num_rows)]
+
+def identity_matrix(n: int) -> Matrix:
+    """Returns the n x n identity matrix"""
+    return make_matrix(n, n, lambda i, j: 1 if i == j else 0)
+
+assert identity_matrix(5) == [[1, 0, 0, 0, 0],
+                            [0, 1, 0, 0, 0],
+                            [0, 0, 1, 0, 0],
+                            [0, 0, 0, 1, 0],
+                            [0, 0, 0, 0, 1]]
